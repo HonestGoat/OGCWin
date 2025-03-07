12c5a8157c8585ee42018b4ca1b1c301a431e42d
Honest Goat <132858305+HonestGoat@users.noreply.github.com>
2025-03-07 21:04:23 +1100

    Add files via upload

--- OGCWin.bat
+++ OGCWin.bat
@@ -1,18 +1,3 @@
 @echo off
-:: Set PowerShell script path
-set "parentFolder=C:\ProgramData\OGC Windows Utility"
-set "scriptsFolder=%parentFolder%\scripts"
-set "OGClaunch=%scriptsFolder%\launch.ps1"
-
-:: Check if PowerShell is running as Administrator
-net session >nul 2>&1
-if %errorLevel% neq 0 (
-    echo Requesting administrative privileges...
-    powershell -Command "Start-Process cmd -ArgumentList '/c %~s0' -Verb RunAs"
-    exit /b
-)
-
-:: Launch PowerShell with black background, custom title, and admin rights
-powershell -NoExit -ExecutionPolicy Bypass -NoProfile -Command ^
-"& { $host.UI.RawUI.WindowTitle = 'OGC Windows Utility Launcher'; $host.UI.RawUI.BackgroundColor = 'Black'; $host.UI.RawUI.ForegroundColor = 'White'; Clear-Host; Start-Process powershell.exe -ArgumentList '-NoExit -ExecutionPolicy Bypass -NoProfile -File \"%OGClaunch%\"' -Verb RunAs }"
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -Command "Start-Process powershell.exe -Verb RunAs -ArgumentList '-ExecutionPolicy Bypass -NoProfile -NoExit -File \"C:\ProgramData\OGC Windows Utility\scripts\launch.ps1\"' -WindowStyle Normal; $host.UI.RawUI.BackgroundColor = 'Black'"
 exit
