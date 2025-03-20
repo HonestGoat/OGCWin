b87f97d2feb9b6f9df6256e37028b88a47386641
Honest Goat <132858305+HonestGoat@users.noreply.github.com>
2025-03-20 11:47:07 +1100

    Update OGCWin.bat

--- OGCWin.bat
+++ OGCWin.bat
@@ -1,4 +1,5 @@
 @echo off
+title OGCWin Mode Selector
 "%ProgramFiles%\PowerShell\7\pwsh.exe" -ExecutionPolicy Bypass -Command ^
 "Start-Process -FilePath pwsh.exe -Verb RunAs -ArgumentList '-NoExit -ExecutionPolicy Bypass -NoProfile -File \"C:\ProgramData\OGC Windows Utility\scripts\OGCMode.ps1\"'"
 exit
